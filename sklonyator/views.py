64babc399e84c1ec441d1bf8d8551b46c84e83f8
denya-vs <denya_vs@mail.ru>
2014-05-12 17:12:11 +0000

    first commit

---
+++ sklonyator/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse
+import pymorphy2
+
+def api(request):
+	inpunt_word = request.GET['s']
+	case = request.GET['case']
+	morph = pymorphy2.MorphAnalyzer()
+	variations = morph.parse(inpunt_word)
+	if len(variations) > 1:
+		for (i, item) in enumerate(variations):
+			if item.normal_form == inpunt_word:
+				word = variations[i]
+				break
+	else:
+		word = variations[0]
+	number = request.GET.get('number', word.tag.number)
+	result = word.inflect({number, case})
+	return HttpResponse(result.word)
+
+def home(request):
+	return render(request, 'home.html')
